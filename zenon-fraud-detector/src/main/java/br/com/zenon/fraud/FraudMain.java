119301f275acc5c2ff9a9916adcce49fd8e5c9ca
Daniel Mesquita <danielmesquitadev@gmail.com>
2026-07-22 21:22:41 -0300

    feat: enable transaction ingestor for csv files

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/FraudMain.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/FraudMain.java
@@ -1,38 +1,16 @@
 package br.com.zenon.fraud;
 
-import java.math.BigDecimal;
+import java.io.IOException;
+import java.util.List;
 
 public class FraudMain {
-  public static void main(String[] args) {
-    Transaction transaction1 =
-        new Transaction(
-            1,
-            "PAYMENT",
-            new BigDecimal("9839.64"),
-            "C1231006815",
-            new BigDecimal("170136.0"),
-            new BigDecimal("160296.36"),
-            "M1979787155",
-            new BigDecimal("0.0"),
-            new BigDecimal("0.0"),
-            0,
-            0);
+  public static void main(String[] args) throws IOException {
 
-    Transaction transaction2 =
-        new Transaction(
-            743,
-            "CASH_OUT",
-            new BigDecimal("850002.52"),
-            "C1280323807",
-            new BigDecimal("850002.52"),
-            new BigDecimal("0.0"),
-            "C873221189",
-            new BigDecimal("6510099.11"),
-            new BigDecimal("7360101.63"),
-            1,
-            0);
+    TransactionIngestor transactionIngestor = new TransactionIngestor();
+    List<Transaction> transactions = transactionIngestor.getTransactions("data/pay_data.csv");
 
-    System.out.println("Transaction 1: " + transaction1);
-    System.out.println("Transaction 2: " + transaction2);
+    for (int index = 0; index < 10; index++) {
+      System.out.println("Transaction " + (index + 1) + ": " + transactions.get(index));
+    }
   }
 }
---
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/TransactionIngestor.java
@@ -0,0 +1,41 @@
+package br.com.zenon.fraud;
+
+import java.io.*;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+public class TransactionIngestor {
+  public TransactionIngestor() {}
+
+  public List<Transaction> getTransactions(String filePath) throws IOException {
+    List<Transaction> transactions = new ArrayList<>();
+    Reader reader = Files.newBufferedReader(Paths.get(filePath));
+    try (CSVParser parser =
+        CSVFormat.DEFAULT.builder().setHeader().setSkipHeaderRecord(true).get().parse(reader)) {
+      for (int i = 0; i < 1000; i++) {
+        CSVRecord record = parser.iterator().next();
+        Transaction transaction =
+            new Transaction(
+                Integer.parseInt(record.get("step")),
+                record.get("type"),
+                new BigDecimal(record.get("amount")),
+                record.get("nameOrig"),
+                new BigDecimal(record.get("oldbalanceOrg")),
+                new BigDecimal(record.get("newbalanceOrig")),
+                record.get("nameDest"),
+                new BigDecimal(record.get("oldbalanceDest")),
+                new BigDecimal(record.get("newbalanceDest")),
+                Integer.parseInt(record.get("isFraud")),
+                Integer.parseInt(record.get("isFlaggedFraud")));
+        transactions.add(transaction);
+      }
+    }
+    return transactions;
+  }
+}
