9594586fd7f74c4886750f16698cfcb397bcefe8
Daniel Mesquita <danielmesquitadev@gmail.com>
2026-07-23 19:18:00 -0300

    feat: set validation for Transaction

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/Transaction.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/Transaction.java
@@ -1,16 +1,18 @@
 package br.com.zenon.fraud;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 public record Transaction(
-    int step,
-    String type,
-    BigDecimal amount,
-    String nameOrig,
-    BigDecimal oldbalanceOrg,
-    BigDecimal newbalanceOrig,
-    String nameDest,
-    BigDecimal oldbalanceDest,
-    BigDecimal newbalanceDest,
-    int isFraud,
-    int isFlaggedFraud) {}
+    @NotNull @Min(value = 1, message = "Step must me greater than 0") int step,
+    @NotNull String type,
+    @NotNull @Min(value = 0, message = "Value should not be negative") BigDecimal amount,
+    @NotNull String nameOrig,
+    @NotNull @Min(value = 0, message = "Value should not be negative") BigDecimal oldbalanceOrg,
+    @NotNull @Min(value = 0, message = "Value should not be negative") BigDecimal newbalanceOrig,
+    @NotNull String nameDest,
+    @NotNull @Min(value = 0, message = "Value should not be negative") BigDecimal oldbalanceDest,
+    @NotNull @Min(value = 0, message = "Value should not be negative") BigDecimal newbalanceDest,
+    @NotNull int isFraud,
+    @NotNull int isFlaggedFraud) {}
---
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/TransactionTypeEnum.java
@@ -0,0 +1,9 @@
+package br.com.zenon.fraud;
+
+public enum TransactionTypeEnum {
+  PAYMENT,
+  CASH_OUT,
+  CASH_IN,
+  TRANSFER,
+  DEBIT
+}
