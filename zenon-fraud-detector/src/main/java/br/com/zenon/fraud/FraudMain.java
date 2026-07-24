950b75cb49a261c3107cc3968b858a72c1fd5fa3
Daniel Mesquita <danielmesquitadev@gmail.com>
2026-07-24 15:36:34 -0300

    feat: implement exception for validation

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/FraudMain.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/FraudMain.java
@@ -2,16 +2,26 @@ package br.com.zenon.fraud;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class FraudMain {
+  private static final Logger logger = Logger.getLogger(FraudMain.class.getName());
+
   public static void main(String[] args) throws IOException {
 
     TransactionIngestor transactionIngestor = new TransactionIngestor();
-    List<Transaction> transactions =
-        transactionIngestor.getTransactions("data/paysim_with_bad_data.csv");
 
-    for (int index = 0; index < 10; index++) {
-      System.out.println("Transaction " + (index + 1) + ": " + transactions.get(index));
+    logger.info("Application started");
+
+    try (Scanner scanner = new Scanner(System.in)) {
+      String filePath = scanner.nextLine();
+
+      List<Transaction> transactions = transactionIngestor.getTransactions(filePath);
+
+      for (Transaction transaction : transactions) {
+        System.out.println("Transaction " + transaction);
+      }
     }
   }
 }
--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/TransactionIngestor.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/TransactionIngestor.java
@@ -8,10 +8,7 @@ import java.io.*;
 import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
@@ -29,94 +26,44 @@ public class TransactionIngestor {
       Iterator<CSVRecord> iterator = parser.iterator();
       for (int i = 0; i < 1000 && iterator.hasNext(); i++) {
         CSVRecord record = iterator.next();
-        try {
+        try (ValidatorFactory factory =
+            Validation.byDefaultProvider()
+                .configure()
+                .messageInterpolator(new ParameterMessageInterpolator())
+                .buildValidatorFactory()) {
+          Validator validator = factory.getValidator();
           Transaction transaction = extractTransactionLineFromCSV(record);
-          transactions.add(extractTransactionLineFromCSV(record));
+          Set<ConstraintViolation<Transaction>> violations = validator.validate(transaction);
+
+          if (!violations.isEmpty()) {
+            String message =
+                violations.stream()
+                    .map(ConstraintViolation::getMessage)
+                    .collect(Collectors.joining());
+            throw new IllegalArgumentException("Validation failed for transaction: " + message);
+          } else {
+            transactions.add(transaction);
+          }
         } catch (IllegalArgumentException e) {
-          System.err.println(
-              "Error parsing record at line "
-                  + record.getRecordNumber()
-                  + ": "
-                  + e
-                  + ". Skipping this record.");
-          continue;
+          System.err.println("Erro " + Arrays.toString(record.values()) + ": " + e);
         }
       }
     }
     return transactions;
   }
 
-  public List<Transaction> getInvalidTransactions(String filePath) throws IOException {
-    List<Transaction> invalidTransactions = new ArrayList<>();
-    try (ValidatorFactory factory =
-        Validation.byDefaultProvider()
-            .configure()
-            .messageInterpolator(new ParameterMessageInterpolator())
-            .buildValidatorFactory()) {
-      Validator validator = factory.getValidator();
-      Reader reader = Files.newBufferedReader(Paths.get(filePath));
-      try (CSVParser parser =
-          CSVFormat.DEFAULT.builder().setHeader().setSkipHeaderRecord(true).get().parse(reader)) {
-        Iterator<CSVRecord> iterator = parser.iterator();
-        for (int i = 0; i < 1000 && iterator.hasNext(); i++) {
-          CSVRecord record = iterator.next();
-          try {
-            Transaction transaction = extractTransactionLineFromCSV(record);
-            Set<ConstraintViolation<Transaction>> violations = validator.validate(transaction);
-
-            if (!violations.isEmpty()) {
-              String message =
-                  violations.stream()
-                      .map(ConstraintViolation::getMessage)
-                      .collect(Collectors.joining());
-              throw new IllegalArgumentException("Validation failed for transaction: " + message);
-            } else {
-              invalidTransactions.add(transaction);
-            }
-          } catch (IllegalArgumentException e) {
-            System.err.println(
-                "Error parsing record at line "
-                    + record.getRecordNumber()
-                    + ": "
-                    + e
-                    + ". Skipping this record.");
-            continue;
-          }
-        }
-      }
-    }
-    return invalidTransactions;
-  }
-
   private Transaction extractTransactionLineFromCSV(CSVRecord record) {
-    try (ValidatorFactory factory =
-        Validation.byDefaultProvider()
-            .configure()
-            .messageInterpolator(new ParameterMessageInterpolator())
-            .buildValidatorFactory()) {
-      Validator validator = factory.getValidator();
-      try {
-        Transaction transaction =
-            new Transaction(
-                Integer.parseInt(record.get("step")),
-                TransactionType.valueOf(record.get("type")),
-                new BigDecimal(record.get("amount")),
-                record.get("nameOrig"),
-                new BigDecimal(record.get("oldbalanceOrg")),
-                new BigDecimal(record.get("newbalanceOrig")),
-                record.get("nameDest"),
-                new BigDecimal(record.get("oldbalanceDest")),
-                new BigDecimal(record.get("newbalanceDest")),
-                Integer.parseInt(record.get("isFraud")),
-                Integer.parseInt(record.get("isFlaggedFraud")));
-        Set<ConstraintViolation<Transaction>> violations = validator.validate(transaction);
-      } catch (NumberFormatException e) {
-        throw new IllegalArgumentException(
-            "Invalid number format in record: " + record.toString(), e);
-      } catch (IllegalArgumentException e) {
-        throw new IllegalArgumentException("Invalid enum value in record: " + record.toString(), e);
-      }
-    }
-    return null;
+    return new Transaction(
+        Integer.parseInt(record.get("step")),
+        TransactionType.valueOf(record.get("type")),
+        new BigDecimal(record.get("amount")),
+        record.get("nameOrig"),
+        new BigDecimal(record.get("oldbalanceOrg")),
+        new BigDecimal(record.get("newbalanceOrig")),
+        record.get("nameDest"),
+        new BigDecimal(record.get("oldbalanceDest")),
+        new BigDecimal(record.get("newbalanceDest")),
+        Integer.parseInt(record.get("isFraud")),
+        Integer.parseInt(record.get("isFlaggedFraud")));
   }
 }
